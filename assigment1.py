09e69bb1a9acd50782c7b4a8a127a75ae01cec28
nishchay330 <snishchay92@gmail.com>
2026-06-09 11:53:43 +0530

    Add files via upload

---
+++ assigment1.py
@@ -0,0 +1,62 @@
+# QUESTION 1
+name="nishchay"
+print(name[0])
+print(name[7])
+print(len(name))
+print(name.upper())
+print(name.lower())
+print( name[::-1])
+
+# QUESTION 2
+car="mercedes"
+print(car[0:5])
+print(car[2:5])
+print(car[::-1])
+
+# QUESTION 3
+lst=[1,2,3,4]
+lst.append(5)
+print(lst)
+lst.insert(2,8)
+print(lst)
+lst.remove(3)
+print(lst)
+lst.pop(1)
+print(lst)
+print(lst[::-1])
+lst.sort()
+print(lst)
+print(len(lst))
+print(lst.count(4))
+
+# QUESTION 4
+tpl=(1,2,2,4,5)
+print(len(tpl))
+print(tpl[0])
+print(tpl[-1])
+print(tpl[0:4])
+print(max(tpl))
+print(min(tpl))
+print(sum(tpl))
+
+# QUESTION 5
+tpl=(13,7,"hello") #tuple packing
+print(tpl)
+a,b,c=(13,7,"hello")  #tuple unpacking
+print(a)
+print(b)
+print(c)
+
+# QUESTION 6
+dict={"name":"nishchay","age":"21","course":"BTECH","address":"jaipur"}
+print(dict.keys())
+print(dict.values())
+print(dict.items())
+
+
+
+
+
+
+
+
